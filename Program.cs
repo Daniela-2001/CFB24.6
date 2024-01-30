b0d36c9e17605a30e39e84983bb0a895f1011d2e
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-30 09:23:00 +0000

    CFB24.6

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace aula24._6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int v1, v2,r;
+            v1 = Convert.ToInt32(Console.ReadLine());
+            v2 = Convert.ToInt32(Console.ReadLine());
+            r = soma(v1, v2);
+
+            Console.WriteLine("A soma de {0} e {1} é {2}", v1, v2, r);
+        }
+
+        static int soma(int n1, int n2)
+        {
+            int res = n1 + n2;
+
+            return res;
+        }
+    }
+}
